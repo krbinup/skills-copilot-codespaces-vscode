82a62603a5f98571b3ecae9123bdf3746f1649a2
krbinup <krbinup@gmail.com>
2024-04-06 16:41:49 +0000

    3rd push

---
+++ comments.js
@@ -0,0 +1,22 @@
+// Create web server
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var port = 3000;
+
+http.createServer(function (req, res) {
+    var q = url.parse(req.url, true);
+    var filename = "." + q.pathname;
+    fs.readFile(filename, function (err, data) {
+        if (err) {
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            return res.end("404 Not Found");
+        }
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        return res.end();
+    });
+}).listen(port, function () {
+    console.log('Server running at http://'+port+'/');
+}
+);
